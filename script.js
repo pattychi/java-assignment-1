2128d63a99eafbe5bfd1b8ca8ffb4dd61fb63df3
pattychi <105489664+pattychi@users.noreply.github.com>
2022-06-23 23:09:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let  myName= "Nwosu Patience";
+
+//console.log (myName);
+
+let myHeight= "5ft.6inches"
+//console.log=(myHeight);
+
+let myCountry= "Nigeria"
+//console.log(myCountry);
+
+document.write(myName+ "<br>" + myHeight+ "<br>" + myCountry);
